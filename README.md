f32e014a5f5ea9f66f4ce67e7606f74de47470e1
fabiovalinhos <fabiovalinhos@gmail.com>
2020-07-02 21:59:24 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+##Versão web do xMarket  
---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Ano do footer
+var data = new Date();
+var dataYear = data.getFullYear();
+document.getElementById("dataAno").innerHTML = dataYear;
+
+//////
+
+
+
+
