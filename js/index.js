8629c266682c7d77d6715e3129064fc0f4ef3ed2
fabiovalinhos <fabiovalinhos@gmail.com>
2020-07-03 23:00:19 -0300

    Usando o Vue e axios para acessar uma api

--- js/index.js
+++ js/index.js
@@ -5,6 +5,46 @@ document.getElementById("dataAno").innerHTML = dataYear;
 
 //////
 
+var produtos = [
+  {
+    id: 1,
+    description: "Description 1",
+  },
+  {
+    id: 2,
+    description: "Description 2",
+  },
+  {
+    id: 3,
+    description: "Description 3",
+  },
+  {
+    id: 4,
+    description: "Description 4",
+  },
+  {
+    id: 5,
+    description: "Description 5",
+  },
+  {
+    id: 6,
+    description: "Description 6",
+  },
+];
 
+///////
 
-
+//// Vue
+var app = new Vue({
+  el: "#app",
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then(response => (this.info = response));
+  },
+});
